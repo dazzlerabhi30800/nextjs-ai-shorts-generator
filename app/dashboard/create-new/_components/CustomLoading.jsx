2d2928c85e7e2b0b1e1f32af0ac5d71c10974d2d
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-10-09 21:06:53 +0530

    fixed custom loader styling for small screens

--- app/dashboard/create-new/_components/CustomLoading.jsx
+++ app/dashboard/create-new/_components/CustomLoading.jsx
@@ -10,9 +10,9 @@ import { VideoContext } from "@/app/_context/VideoContext";
 export function CustomLoading({ loading }) {
   const { videoData } = useContext(VideoContext);
   return (
-    <AlertDialog open={loading}>
-      <AlertDialogContent className="bg-transparent border-gray-400 text-center text-xl h-max shadow-none text-white">
-        <AlertDialogTitle>
+    <AlertDialog open={true}>
+      <AlertDialogContent className="bg-transparent w-[90%] rounded-lg border-gray-400 text-center text-xl h-max shadow-none text-white">
+        <AlertDialogTitle className="text-base sm:text-lg md:text-xl ">
           {videoData && Object.keys(videoData).length > 0
             ? `${videoData?.videoScript ? "Video Script is Generated" : ""} ${
                 videoData?.audioUrl ? ", Audio is generated" : ""
@@ -25,7 +25,7 @@ export function CustomLoading({ loading }) {
               }`
             : "Wait, It can take time upto a minute or so, Please Be Patient!!"}
         </AlertDialogTitle>
-        <div className="bg-transparent h-44 w-fit mx-auto">
+        <div className="bg-transparent h-36 sm:h-44 w-fit mx-auto">
           <DotLottieReact
             src="https://lottie.host/427c7f09-66fc-4ef6-af74-25971d91435d/VzboCyw2B7.json"
             loop
