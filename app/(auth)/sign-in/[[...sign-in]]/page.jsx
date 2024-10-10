1fe93a64347712e76a5ad3bff9e26b1e490a6fe3
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-10-10 09:47:09 +0530

    change sign in page img

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -4,14 +4,15 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <div>
+      <div className="hidden md:block h-screen">
         <Image
-          className="w-full h-full object-contain"
+          className="w-full h-full object-cover"
           src="/login.jpg"
           alt="Clerk Logo"
           width={200}
           height={200}
           priority={true}
+          quality={100}
         />
       </div>
       <div className="flex items-center justify-center h-screen">
